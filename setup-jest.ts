fdf33a28014535e69b208466a96692742456c57e
Daniel Marroquin <dev.danielmarroquin25@gmail.com>
2025-05-04 23:22:03 -0500

    Ajustes side bar

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,7 +1,5 @@
-// setup-jest.ts
 import 'jest-preset-angular/setup-jest';
 
-// Opcional: Mocks para el DOM
 Object.defineProperty(window, 'CSS', { value: null });
 Object.defineProperty(document, 'doctype', { value: '<!DOCTYPE html>' });
 Object.defineProperty(window, 'getComputedStyle', {
